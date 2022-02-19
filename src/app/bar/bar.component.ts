9b3b102e8ca284ff7fbf28e0fbad95cd93421c77
Zhannur <jannuraidynuly@gmail.com>
2022-02-19 13:12:42 +0500

    bar component

--- src/app/bar/bar.component.ts
+++ src/app/bar/bar.component.ts
@@ -1,8 +1,24 @@
 import { Component } from "@angular/core";
 
+interface navProps {
+    name: string;
+    icon: boolean;
+}
+
 @Component({
     selector: "bar",
     templateUrl: "./bar.component.html",
     styleUrls: ["./bar.component.sass"]
 })
-export class BarComponent {}
+export class BarComponent {
+    logoText: string = "Test Task Project";
+    navLinks: navProps[] = [
+        {name: 'Home', icon: false},
+        {name: 'About', icon: false},
+        {name: 'Servising', icon: false},
+        {name: 'Portfolio', icon: false},
+        {name: 'Blog', icon: false},
+        {name: "Contact us", icon: false},
+        {name: "bi bi-search", icon: true}
+    ]
+}
